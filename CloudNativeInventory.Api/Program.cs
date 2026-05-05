c57a6152bf04b247c7db9c4b40931f1b78356b3f
Lisa Ebbhagen <lisa.ebbhagen@gmail.com>
2026-05-05 10:18:36 +0200

    Setup from Notion completed

--- CloudNativeInventory.Api/Program.cs
+++ CloudNativeInventory.Api/Program.cs
@@ -1,41 +1,46 @@
+using CloudNativeInventory.Api.Data;
+using CloudNativeInventory.Api.Models;
+using Microsoft.EntityFrameworkCore;
+// using Azure.Identity; // TODO (Del 4): Krävs för Key Vault
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddOpenApi(); // .NET 9 OpenAPI
+
+// TODO (Del 4 i "Tips och förslag"): Konfigurera Azure Key Vault
+// Använd Managed Identity för att hämta hemligheter i produktion.
+// if (builder.Environment.IsProduction())
+// {
+//     var keyVaultUrl = new Uri(builder.Configuration["KeyVaultUrl"]!);
+//     builder.Configuration.AddAzureKeyVault(keyVaultUrl, new DefaultAzureCredential());
+// }
+
+// Vi använder InMemory-databas lokalt
+builder.Services.AddDbContext<InventoryDbContext>(options =>
+    options.UseInMemoryDatabase("InventoryDb"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
-var summaries = new[]
+// Seeda data (se till att vi inte dubblar om appen startas om i samma process)
+using (var scope = app.Services.CreateScope())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var db = scope.ServiceProvider.GetRequiredService<InventoryDbContext>();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (!db.Products.Any())
+    {
+        db.Products.Add(new Product { Id = 1, Name = "Laptop", Price = 9999, StockQuantity = 10 });
+        db.SaveChanges();
+    }
+}
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
